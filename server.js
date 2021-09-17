4b1ee07d5cfd1a4ecc441a03dc1a401c9b2f59ab
abdelrazekAli <dev.abdelrazek@gmail.com>
2021-09-17 18:59:34 +0200

    updated server

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("./client/react-example/build/"));
 
-app.get("/*", (req, res) => {
-  res.sendFile("index.html", {
-    root: `${__dirname}/client/react-example/build/`,
-  });
-});
-
 app.use("/api/posts", postRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
